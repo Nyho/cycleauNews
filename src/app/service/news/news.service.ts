42f26c77aced5cccc94491faf0c44e67f6264322
Hugo <hugo.cymoz@gmail.com>
2022-06-12 19:55:48 +0200

    Update news service && HTTPClientModule import

---
+++ src/app/service/news/news.service.ts
@@ -0,0 +1,20 @@
+/* eslint-disable no-trailing-spaces */
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+
+export class NewsService {
+  constructor() {
+
+  }
+
+  getActivity() {
+
+  }
+
+  getActivities() {
+
+  }
+}
